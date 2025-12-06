76358e8b084d66f69c8e0f36b4a99f8978385e2a
Dr.Abc <me@drabc.net>
2025-12-06 20:14:04 +0800

    1

--- NuggetMod/Enum/Common/DeadFlag.cs
+++ NuggetMod/Enum/Common/DeadFlag.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NuggetMod.Enum.Common;
+﻿namespace NuggetMod.Enum.Common;
 
 /// <summary>
 ///  edict->deadflag values
